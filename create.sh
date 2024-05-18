701923d57869b459de4b3be85cc1680f09ba4dc4
Eriabinin777 <95548107+Eriabinin777@users.noreply.github.com>
2024-05-18 03:13:26 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,8 @@
+curl -L https://istio.io/downloadIstio | sh -
+export PATH="$PATH:$PWD/istio-1.21.2/bin"
+istioctl install -y --set profile=demo --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY
+kubectl label namespace default istio-injection=enabled
+
 docker build -t eriabinin777/my-app -f dockerfileapp .
 docker build -t eriabinin777/my-client -f dockerfileclient .
 docker push eriabinin777/my-app
@@ -6,5 +11,9 @@ docker push eriabinin777/my-client
 kubectl apply -f /Users/rev777/Desktop/Documents/BIT_PPPRP/myapp_deployment.yaml
 kubectl apply -f /Users/rev777/Desktop/Documents/BIT_PPPRP/myservice.yaml
 kubectl apply -f /Users/rev777/Desktop/Documents/BIT_PPPRP/myclient_deployment.yaml
+
+kubectl apply -f /Users/rev777/Desktop/Documents/BIT_PPPRP/mygateway.yaml
+kubectl apply -f /Users/rev777/Desktop/Documents/BIT_PPPRP/myvirtual_service.yaml
+kubectl apply -f /Users/rev777/Desktop/Documents/BIT_PPPRP/myentry_service.yaml
 
 minikube tunnel
